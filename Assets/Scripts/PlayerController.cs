27c6beacca1b9b852bb8ddd10e02628a866208fe
Asvaka <78432238+Asvaka@users.noreply.github.com>
2024-08-16 12:46:00 -0700

    New Stretching System
    This one fixes jitter, so we're gonna go with this one

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,61 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D headRigidbody2D;
+    private Transform headTransform;
 
-    private Vector3 tempScale;
-    private Vector3 tempPosition;
     private GameObject body;
     private float speed = 10;
     private bool stretching = false;
+    private Vector3 initialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         headRigidbody2D = transform.GetChild(0).GetComponent<Rigidbody2D>();
-        tempScale = transform.localScale;
+        headTransform = transform.GetChild(0).GetComponent<Transform>();
         body = transform.GetChild(1).gameObject;
+        initialPosition = headTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (true)//Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        {
-            float xChange = Time.deltaTime * Input.GetAxis("Horizontal") * speed;
-            float yChange = Time.deltaTime * Input.GetAxis("Vertical") * speed;
+        body.transform.localScale = headTransform.position - initialPosition - new Vector3(0.5f, 0.5f, 0);
+        body.transform.localPosition = (headTransform.position - initialPosition - new Vector3(0.5f, 0.5f, 0)) / 2;
 
-            tempScale = body.transform.localScale;
-            tempScale.x += xChange;
-            tempScale.y += yChange;
-
-            tempPosition = body.transform.localPosition;
-            tempPosition.x += xChange / 2;
-            tempPosition.y += yChange / 2;
-        }
-
-        //transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * speed;
         headRigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
-
-
-        if (Input.GetKey(KeyCode.I))
-        {
-            float xChange = Time.deltaTime;
-            float yChange = Time.deltaTime;
-
-            tempScale = body.transform.localScale;
-            tempScale.x += xChange;
-            tempScale.y += yChange;
-
-            tempPosition = body.transform.localPosition;
-            tempPosition.x += xChange / 2;
-            tempPosition.y += yChange / 2;
-        }
-        body.transform.localScale = tempScale;
-        body.transform.localPosition = tempPosition;
-    }
-
-    private void FixedUpdate()
-    {
-        
     }
 }
