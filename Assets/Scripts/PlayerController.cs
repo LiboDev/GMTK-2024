82ab98aea1c729f578f5e6ba1ca3ab83ea08af37
Asvaka <78432238+Asvaka@users.noreply.github.com>
2024-08-18 13:09:02 -0700

    Removed Remnants of Old Movement System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,28 +19,9 @@ public class PlayerController : MonoBehaviour
 
     //Variables related to the body stretching
     private Vector3 startPos;
-    private bool canStretch = true;
-    private bool stretching = false;
-    private bool stretched = false;
-    private string xStretchDir = "none";
-    private string yStretchDir = "none";
-    private bool xStretched = false;
-    private bool yStretched = false;
 
     //Variables relating to the body returning to the head
     [SerializeField] private float returnTimeModifier = 1;
-    Vector2 returnTarget = new Vector2(-1, -1);
-    private bool bodyReturning = false;
-    private float bodyReturnStartTime;
-    private Vector3 startBodyScale;
-
-    //Auxillary variables for manipulating the body
-    //The head's initial position before a stretch
-    private Vector3 initialPosition;
-    //If the head is to the right of the initial position, body is flipped
-    [SerializeField] private bool bodyXFlipped = false;
-    //If the head is above the initial position, body is flipped
-    private bool bodyYFlipped = false;
 
     //stats
     [SerializeField] private int playerSize = 25;
@@ -74,8 +55,6 @@ public class PlayerController : MonoBehaviour
         playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera").GetComponent<CinemachineVirtualCamera>();
         body = transform.GetChild(1).gameObject;
         bodySpriteRenderer = body.GetComponent<SpriteRenderer>();
-        //Save the head's initial position
-        initialPosition = headTransform.localPosition;
 
         startPos = headTransform.position;
 
@@ -95,8 +74,6 @@ public class PlayerController : MonoBehaviour
         {
             gooBar.value = (body.transform.localScale.x * body.transform.localScale.y) / playerSize;
         }
-
-        //PlayerMovement();
 
         body.transform.position = (headTransform.position + startPos) / 2;
         body.transform.localScale = new Vector3(Mathf.Abs(headTransform.position.x - startPos.x) + 1, Mathf.Abs(headTransform.position.y - startPos.y) + 1, 0);
@@ -123,16 +100,6 @@ public class PlayerController : MonoBehaviour
                 FireLeft();
                 Shoot();
             }
-        }
-
-
-        if (stretching || stretched)
-        {
-            bodySpriteRenderer.color = new Color(bodySpriteRenderer.color.r, bodySpriteRenderer.color.g, bodySpriteRenderer.color.b, 0.75f);
-        }
-        else
-        {
-            bodySpriteRenderer.color = new Color(bodySpriteRenderer.color.r, bodySpriteRenderer.color.g, bodySpriteRenderer.color.b, 1);
         }
     }
 
@@ -314,283 +281,6 @@ public class PlayerController : MonoBehaviour
                 GameObject temp = Instantiate(bulletPrefab, bulletPos, Quaternion.LookRotation(Vector3.forward, Vector3.left));
                 temp.GetComponent<PlayerBullet>().Initialize(Vector2.left, bulletDamage, bulletKnockback, range);
             }
-        }
-    }
-
-    private void PlayerMovement()
-    {
-        //If the player presses any movement key while not currently stretched, set all the movement stuff in that/those direction(s)
-        if (!stretched && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
-        {
-            if (xStretchDir == "none" && !xStretched)
-            {
-                if (Input.GetKey(KeyCode.D))
-                {
-                    xStretchDir = "right";
-                    bodyXFlipped = true;
-                    returnTarget = new Vector2(1, returnTarget.y);
-                }
-                if (Input.GetKey(KeyCode.A))
-                {
-                    xStretchDir = "left";
-                    bodyXFlipped = false;
-                    returnTarget = new Vector2(-1, returnTarget.y);
-                }
-                stretching = true;
-            }
-
-            if (yStretchDir == "none" && !yStretched)
-            {
-                if (Input.GetKey(KeyCode.W))
-                {
-                    yStretchDir = "up";
-                    bodyYFlipped = true;
-                    returnTarget = new Vector2(returnTarget.x, 1);
-                }
-                if (Input.GetKey(KeyCode.S))
-                {
-                    yStretchDir = "down";
-                    bodyYFlipped = false;
-                    returnTarget = new Vector2(returnTarget.x, -1);
-                }
-                stretching = true;
-            }
-
-
-        }
-
-        if (stretching && (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)))
-        {
-            switch (xStretchDir)
-            {
-                case "right":
-                    if (Input.GetKeyUp(KeyCode.D))
-                    {
-                        if (yStretchDir != "none")
-                        {
-                            xStretchDir = "none";
-                            xStretched = true;
-                        }
-                        else
-                        {
-                            StopBodyStretching();
-                        }
-                    }
-                    break;
-                case "left":
-                    if (Input.GetKeyUp(KeyCode.A))
-                    {
-                        if (yStretchDir != "none")
-                        {
-                            xStretchDir = "none";
-                            xStretched = true;
-                        }
-                        else
-                        {
-                            StopBodyStretching();
-                        }
-                    }
-                    break;
-            }
-            switch (yStretchDir)
-            {
-                case "up":
-                    if (Input.GetKeyUp(KeyCode.W))
-                    {
-                        if (xStretchDir != "none")
-                        {
-                            yStretchDir = "none";
-                            yStretched = true;
-                        }
-                        else
-                        {
-                            StopBodyStretching();
-                        }
-                    }
-                    break;
-                case "down":
-                    if (Input.GetKeyUp(KeyCode.S))
-                    {
-                        if (xStretchDir != "none")
-                        {
-                            yStretchDir = "none";
-                            yStretched = true;
-                        }
-                        else
-                        {
-                            StopBodyStretching();
-                        }
-                    }
-                    break;
-            }
-        }
-
-        if (bodyReturning)
-        {
-            float fracComplete = (Time.time - bodyReturnStartTime) / 0.5f;
-            body.transform.localScale = Vector2.Lerp(startBodyScale, returnTarget, fracComplete);
-            if (bodyXFlipped && bodyYFlipped)
-            {
-                body.transform.localPosition = headTransform.localPosition - ((body.transform.localScale) / 2) + new Vector3(0.5f, 0.5f, 0);
-            }
-            else if (bodyXFlipped)
-            {
-                body.transform.localPosition = headTransform.localPosition - ((body.transform.localScale) / 2) + new Vector3(0.5f, -0.5f, 0);
-            }
-            else if (bodyYFlipped)
-            {
-                body.transform.localPosition = headTransform.localPosition - ((body.transform.localScale) / 2) + new Vector3(-0.5f, 0.5f, 0);
-            }
-            else
-            {
-                body.transform.localPosition = headTransform.localPosition - ((body.transform.localScale) / 2) - new Vector3(0.5f, 0.5f, 0);
-            }
-
-            if (fracComplete >= 1)
-            {
-                xStretchDir = "none";
-                yStretchDir = "none";
-                stretched = false;
-                bodyReturning = false;
-                canStretch = true;
-                initialPosition = headTransform.localPosition;
-                body.transform.localPosition = initialPosition;
-                body.transform.localScale = Vector2.one;
-            }
-        }
-        else if (canStretch)
-        {
-            Vector3 changeFromStartingPos = headTransform.localPosition - initialPosition - new Vector3(0.5f, 0.5f, 0);
-            if (bodyXFlipped && bodyYFlipped)
-            {
-                changeFromStartingPos = headTransform.localPosition - initialPosition + new Vector3(0.5f, 0.5f, 0);
-                body.transform.localScale = changeFromStartingPos + new Vector3(0.5f, 0.5f, 0);
-                body.transform.localPosition = ((changeFromStartingPos) / 2) - new Vector3(0.25f, 0.25f, 0);
-            }
-            else if (bodyXFlipped)
-            {
-                changeFromStartingPos = headTransform.localPosition - initialPosition + new Vector3(0.5f, -0.5f, 0);
-                body.transform.localScale = changeFromStartingPos + new Vector3(0.5f, -0.5f, 0);
-                body.transform.localPosition = ((changeFromStartingPos) / 2) - new Vector3(0.25f, -0.25f, 0);
-            }
-            else if (bodyYFlipped)
-            {
-                changeFromStartingPos = headTransform.localPosition - initialPosition + new Vector3(-0.5f, 0.5f, 0);
-                body.transform.localScale = changeFromStartingPos + new Vector3(-0.5f, 0.5f, 0);
-                body.transform.localPosition = ((changeFromStartingPos) / 2) - new Vector3(-0.25f, 0.25f, 0);
-            }
-            else
-            {
-                body.transform.localScale = changeFromStartingPos - new Vector3(0.5f, 0.5f, 0);
-                body.transform.localPosition = ((changeFromStartingPos) / 2) + new Vector3(0.25f, 0.25f, 0);
-            }
-            body.transform.position += initialPosition;
-        }
-
-        if (!stretched && canStretch)
-        {
-            switch (xStretchDir)
-            {
-                case "right":
-                    headRigidbody2D.velocity = new Vector2(Mathf.Max(Input.GetAxisRaw("Horizontal"), 0), headRigidbody2D.velocity.y);
-                    break;
-                case "left":
-                    headRigidbody2D.velocity = new Vector2(Mathf.Min(Input.GetAxisRaw("Horizontal"), 0), headRigidbody2D.velocity.y);
-                    break;
-                default:
-                    headRigidbody2D.velocity = new Vector2(0, headRigidbody2D.velocity.y);
-                    break;
-            }
-
-            switch (yStretchDir)
-            {
-                case "up":
-                    headRigidbody2D.velocity = new Vector2(headRigidbody2D.velocity.x, Mathf.Max(Input.GetAxisRaw("Vertical"), 0));
-                    break;
-                case "down":
-                    headRigidbody2D.velocity = new Vector2(headRigidbody2D.velocity.x, Mathf.Min(Input.GetAxisRaw("Vertical"), 0));
-                    break;
-                default:
-                    headRigidbody2D.velocity = new Vector2(headRigidbody2D.velocity.x, 0);
-                    break;
-            }
-
-            headRigidbody2D.velocity *= speed;
-        }
-        else if (!stretched)
-        {
-            Vector2 headTarget = new Vector2(1, 1);
-
-            if (bodyXFlipped)
-            {
-                headTarget = new Vector2(body.transform.localPosition.x + (body.transform.localScale.x / 2) - 0.5f, headTarget.y);
-            }
-            else
-            {
-                headTarget = new Vector2(body.transform.localPosition.x + (body.transform.localScale.x / 2) + 0.5f, headTarget.y);
-            }
-
-            if (bodyYFlipped)
-            {
-                headTarget = new Vector2(headTarget.x, body.transform.localPosition.y + (body.transform.localScale.y / 2) - 0.5f);
-            }
-            else
-            {
-                headTarget = new Vector2(headTarget.x, body.transform.localPosition.y + (body.transform.localScale.y / 2) + 0.5f);
-            }
-
-            if (bodyXFlipped && bodyYFlipped)
-            {
-                if (headTransform.localPosition.x >= headTarget.x && headTransform.localPosition.y >= headTarget.y)
-                {
-                    headRigidbody2D.velocity = Vector2.zero;
-                    headTransform.localPosition = headTarget;
-                }
-            }
-            else if (bodyXFlipped)
-            {
-                if (headTransform.localPosition.x >= headTarget.x && headTransform.localPosition.y <= headTarget.y)
-                {
-                    headRigidbody2D.velocity = Vector2.zero;
-                    headTransform.localPosition = headTarget;
-                }
-            }
-            else if (bodyYFlipped)
-            {
-                if (headTransform.localPosition.x <= headTarget.x && headTransform.localPosition.y >= headTarget.y)
-                {
-                    headRigidbody2D.velocity = Vector2.zero;
-                    headTransform.localPosition = headTarget;
-                }
-            }
-            else
-            {
-                if (headTransform.localPosition.x <= headTarget.x && headTransform.localPosition.y <= headTarget.y)
-                {
-                    headRigidbody2D.velocity = Vector2.zero;
-                    headTransform.localPosition = headTarget;
-                }
-            }
-        }
-        else
-        {
-            headRigidbody2D.velocity = Vector2.zero;
-        }
-
-        if ((Mathf.Abs(body.transform.localScale.x) * Mathf.Abs(body.transform.localScale.y)) >= playerSize)
-        {
-            canStretch = false;
-        }
-
-        void StopBodyStretching()
-        {
-            stretching = false;
-            stretched = true;
-            bodyReturning = true;
-            bodyReturnStartTime = Time.time;
-            startBodyScale = body.transform.localScale;
-            xStretched = false;
-            yStretched = false;
         }
     }
 
